4f1830c1312ba457a4d9be21c47ca897eb63a76d
Logesh Shanmugavel <logesh81098@gmail.com>
2025-02-10 10:10:41 +0530

    Deployed S3 bucket to store individuals sample images

---
+++ module/s3/main.tf
@@ -0,0 +1,13 @@
+##################################################################################################################################################
+#                                                      Source S3 image
+##################################################################################################################################################
+
+#S3 bucket to store the source photographs that will be utilized by the Rekognition service to identify the individuals
+
+resource "aws_s3_bucket" "source-bucket" {
+  bucket = "individuals-source-image"
+  tags = {
+    Name = "individuals-source-image"
+    Project = "Face Recognition"
+  }
+}
